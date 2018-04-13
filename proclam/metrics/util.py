951172fcd4251602ad8fc9128942a31a92b16ab4
Alex Malz <aimalz@nyu.edu>
2018-04-13 10:37:54 -0400

    fixed a bug, added comparison with scikit-learn

--- proclam/metrics/util.py
+++ proclam/metrics/util.py
@@ -24,13 +24,16 @@ def truth_reformatter(truth, prediction=None):
     -----
     Does not yet handle number of classes in truth not matching number of classes in prediction
     """
+    N = len(truth)
+    indices = range(N)
+
     if prediction is None:
-        prediction_shape = (len(truth), np.max(truth) + 1)
+        prediction_shape = (N, np.max(truth) + 1)
     else:
         prediction, truth = np.asarray(prediction), np.asarray(truth)
         prediction_shape = np.shape(prediction)
 
     truth_reformatted = np.zeros(prediction_shape)
-    truth_reformatted[:, truth] = 1.
+    truth_reformatted[indices, truth] = 1.
 
     return truth_reformatted
